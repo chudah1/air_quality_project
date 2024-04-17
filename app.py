2dacbf9f18e352966a52902269a3dd4c536e79cb
Leanne Annor-Adjaye <leanneannor-adjaye@Leannes-MacBook-Pro.local>
2024-04-17 15:26:34 +0000

    fixed threshold

--- app.py
+++ app.py
@@ -7,14 +7,44 @@ import os
 from dotenv import load_dotenv
 
 from utils import *
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 load_dotenv() 
+
+
 
 app = Flask(__name__, static_url_path='/static')
 
 
+def check_thresholds_and_alert():
+    users = db.collection("users").get()
+    for user in users:
+        user_data = user.to_dict()
+        if "thresholds" not in user_data:
+            continue
+        thresholds = user_data["thresholds"]
+        email = user_data["email"]
+        for threshold in thresholds:
+            location = threshold["location"]
+            threshold_value = threshold["threshold"]
+            air_quality_data = fetch_air_quality(location)
+            if air_quality_data > threshold_value:
+                send_emails(email, location, air_quality_data)
+
+scheduler = BackgroundScheduler()
+
+
+scheduler.start()
+
+
+scheduler.add_job(check_thresholds_and_alert, trigger="interval", minutes=1)
+
+
+
+
 config = {
-"apiKey": os.environ['API_KEY'],
+"apiKey": os.getenv('API_KEY'),
 "authDomain": "airquality-d611e.firebaseapp.com",
 "projectId": "airquality-d611e",
 "storageBucket": "airquality-d611e.appspot.com",
@@ -28,17 +58,16 @@ firebase = pyrebase.initialize_app(config)
 
 auth = firebase.auth()
 
-app.secret_key = os.environ['SECRET_KEY']
+app.secret_key = os.getenv('SECRET_KEY')
 
-firebase_key = os.environ["FIREBASE_KEY"]
-
+firebase_key = os.getenv("FIREBASE_KEY")
 
 #initialize the firestore database
 cred = credentials.Certificate(
     { 
 "type": "service_account",
   "project_id": "airquality-d611e",
-  "private_key_id": os.environ['PRIVATE_KEY'],
+  "private_key_id": os.getenv('PRIVATE_KEY'),
   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDYXWDcbKTbX5P/\nrncxk7f47GmzBWwvKeQUzmeHsrxQ39piRBf3kTfcp/MN9tpmkn8wtsF030u7m00S\nDk99SsRJTc480hSHsiCwjyIAKpD49IrBOUJugLOzXI2her12CV7wtLUfKylbKrYk\nPXHEXQZAUCAnK/oIyi7Nd1GXPF/vTEjUpPyc1xW86qkuQ59et+USWlZR7XOWtlY1\nKIaWKC949UDsjO2RRfQjNeMvnYDfBsKASOwFUDfiJ3HLHk4bxX3s939u96DPJyhI\nBfEzzCJWCreS+Mu33UPXZKvU3CLdsLQ7mraETP0fJBSmfubVeLx8lkLMWVSymvwZ\nW6Uj47qrAgMBAAECggEAI/30nPZdvjh6EDBfl5mxIvGA68zdIENhs6xaQJKVDQcP\n9wTIz+ASYNx3bD1CO4nnKp1cNSroGD9PZM+InZkQaflNc4Sm8aPKGFTXHRv3ndJg\nOqBh0qwKOK4OyWv3lGkeqAAHmTW+3XGPdxvZjwbCgXRSxPFl2Ix5mKhepD/g3ZeO\negqvIor57mNtDMSyYi1i38NupCnwUNoEABud/eQgJgzpzzDluhLaHKVPrFIuVCS7\nd94g5GUJ+pDaev4BdZlBKXQJCrpDT+carISARYv3z/qR9T84ezq47al82FBZ3vkU\ndWRx8U3WtSG8GZhiQFB09dfwK57Px2sigF5qUaY+cQKBgQD/+tNgOlNWrT3B5ceG\ntwj3i2t3I+4xagFFRdpVeACODT6gURtcabcEN36oKkbIO9Itx5t2tvSg3Smqi25z\nq1NcdvoH75wroUDl2rRIb44QTPJks2gXHtJByDUcTo5SvKMLdq94f9XCMJW51hzy\nbDuQvUo5P9arsbMYP9lzsKa73QKBgQDYYcB9B/c4JiOkKs8+BIlI/FXmqArk7s0X\ndbmYizO/DQJiJI++Q5UJwz8W6g5hsyXv9UU6Qp5RGX63bPLcL6p4L8BC4UsRAGG2\nw/CRAHHK7vlgQyXeeqENyo/THDtCCEf0aroiOIG/9SrnQG1S1sn8ToQElMDCr/cz\nvy/xe93MJwKBgQC7nnl9Z2Kb/iBs1OE4kUGGRu7+hRxZpYvG3VsZF/q4I2cKlEgM\npQNamN5BnbMBoi78FPiSu6hzm4allMrhBurzs9SxKmN56xgpIPLQwMsMkYl8W67o\ne1O6mWasF7vjUpXimhwkovXm/jtP2WqgGMpT3Rng+jR65aUSGgJk9E5RFQKBgD+4\n6droUaGzeaOLB+UXqOZZiWKX8j1hJfzWqoWF2QNWbXFBmyNI+8cYPKge3YVgILf9\n3xk9LSps+6hA6XR2hRlH1rLbbiPfnOcZ5OqO2vQMVFxI6goOywcotBpTog/cKHSO\nSapyQaOqK6xWiNtwbaNj6/T9aZbsAzy1/QR8meDZAoGAFGfq5uoYssH5eseQa4Qo\nqxlqIiQIgNV6y9J0pYMeeYvCf8wUnQ+aw1ISlq1B/6HihljhibVMRLE7Azmt9rpg\n0Z6U0uyz1M61XmhWsp8KUSiWqgLnbjNAe5zmKW0ukMeDULscNUMZ1Y87XGlOeHZ1\nm7mFU0zVi6G0LGOK88IaNTY=\n-----END PRIVATE KEY-----\n",
   "client_email": "firebase-adminsdk-yjju3@airquality-d611e.iam.gserviceaccount.com",
   "client_id": "112749579766246350632",
@@ -99,7 +128,10 @@ def threshold(user_id):
         location = request.form.get("location")
         threshold = request.form.get("threshold")
         thresholds = {"location":location, "threshold":threshold}
-        users.doc(user_id).update({"thresholds": firestore.ArrayUnion([thresholds])})
+        users.document(user_id).update({"thresholds": firestore.ArrayUnion([thresholds])})
+
+        return "Successfully created"
+    return "Could not set threshold"
 
 if __name__ == "__main__":
     app.run(debug=True)
